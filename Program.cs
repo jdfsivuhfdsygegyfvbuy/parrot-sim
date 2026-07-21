f8570db5622116f50061d51d6328f91d38380ebe
jdfsivuhfdsygegyfvbuy <tumaevurij0@gmail.com>
2026-07-21 10:27:03 +0300

    Fixed encoding issue

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ using Sim;
 namespace Program;
 class Program {
 	public static void Main(string[] args) {
+		Console.OutputEncoding = System.Text.Encoding.GetEncoding(1200);
 		var sim = new ParrotSim();
 		sim.run();
 	}
